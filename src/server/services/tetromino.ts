aa055da52fa02a6b3c7896562af976a02ef43e0f
Francis Eugene Casibu <projectnevz@gmail.com>
2025-04-06 07:34:21 +0800

    create tetromino class

---
+++ src/server/services/tetromino.ts
@@ -0,0 +1,34 @@
+import { ROTATIONS } from '@shared/constants';
+import {
+  TetrominoType,
+  type ActiveTetromino,
+  type Position,
+  type Rotation,
+} from '@shared/types/game.types';
+import { getTetrominoShape } from '@shared/utils/get-tetromino-shape';
+
+export class Tetromino {
+  private position: Position = { x: 0, y: 0 };
+  private rotation: Rotation = 0;
+
+  constructor(private readonly type: TetrominoType) {}
+
+  public rotate() {
+    const newRotationIndex = ROTATIONS.indexOf(this.rotation) + 1;
+
+    this.rotation = ROTATIONS[newRotationIndex % ROTATIONS.length] as Rotation;
+  }
+
+  public updatePosition(x: number, y: number) {
+    this.position.x = x;
+    this.position.y = y;
+  }
+
+  public getState(): ActiveTetromino {
+    return {
+      type: this.type,
+      shape: getTetrominoShape(this.type, this.rotation),
+      position: this.position,
+    };
+  }
+}
---
+++ src/shared/constants/index.ts
@@ -0,0 +1,1 @@
+export const ROTATIONS = [0, 90, 180, 270] as const;
